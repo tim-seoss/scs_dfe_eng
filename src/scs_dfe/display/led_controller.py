b5c99e50eae3e096893ffd18b40e1bb228525117
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-05-06 09:36:22 +0100

    Added LEDState class.

--- src/scs_dfe/display/led_controller.py
+++ src/scs_dfe/display/led_controller.py
@@ -6,13 +6,14 @@ Created on 30 Apr 2018
 
 import time
 
-from ctypes import c_char
-from multiprocessing import Manager, Value
+from collections import OrderedDict
+from multiprocessing import Manager
 
 from scs_core.sync.interval_timer import IntervalTimer
 from scs_core.sync.synchronised_process import SynchronisedProcess
 
 from scs_dfe.display.led import LED
+from scs_dfe.display.led_state import LEDState
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -25,8 +26,6 @@ class LEDController(SynchronisedProcess):
     __STATE0_PERIOD =   0.8             # seconds
     __STATE1_PERIOD =   0.2             # seconds
 
-    __INITIAL_STATE =   "0"
-
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -37,9 +36,6 @@ class LEDController(SynchronisedProcess):
         manager = Manager()
 
         SynchronisedProcess.__init__(self, manager.list())
-
-        self.__state0 = Value(c_char, self.__INITIAL_STATE.encode())
-        self.__state1 = Value(c_char, self.__INITIAL_STATE.encode())
 
         self.__led = LED()
 
@@ -54,18 +50,22 @@ class LEDController(SynchronisedProcess):
             while timer.true():
                 # values...
                 with self._lock:
-                    state0 = self.__state0.value.decode()
-                    state1 = self.__state1.value.decode()
+                    value = self._value
 
-                # state0...
-                if state0 != self.__led.colour:
-                    self.__led.colour = state0
+                state = LEDState.construct_from_jdict(OrderedDict(value))
+
+                if state is None or not state.is_valid():
+                    continue
+
+                # state 0...
+                if state.colour0 != self.__led.colour:
+                    self.__led.colour = state.colour0
 
                 time.sleep(self.__STATE0_PERIOD)
 
-                # state1...
-                if state1 != self.__led.colour:
-                    self.__led.colour = state1
+                # state 1...
+                if state.colour1 != self.__led.colour:
+                    self.__led.colour = state.colour1
 
         except KeyboardInterrupt:
             pass
@@ -74,13 +74,12 @@ class LEDController(SynchronisedProcess):
     # ----------------------------------------------------------------------------------------------------------------
     # setter for client process...
 
-    def set_states(self, state0, state1):
+    def set_state(self, state):
         with self._lock:
-            self.__state0.value = state0.encode()
-            self.__state1.value = state1.encode()
+            state.as_list(self._value)
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "LEDController:{state0:%s, state1:%s, led:%s}" % (self.__state0, self.__state1, self.__led)
+        return "LEDController:{value:%s, led:%s}" % (self._value, self.__led)
