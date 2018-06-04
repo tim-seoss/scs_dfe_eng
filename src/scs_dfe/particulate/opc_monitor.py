924987bca6c56fc0e90770f153c9d0f4e7db5550
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-06-04 10:39:00 +0100

    Removed TODO.

--- src/scs_dfe/particulate/opc_monitor.py
+++ src/scs_dfe/particulate/opc_monitor.py
@@ -20,8 +20,6 @@ from scs_dfe.particulate.opc_n2 import OPCN2
 from scs_host.lock.lock_timeout import LockTimeout
 
 
-# TODO: discard first (low) reading
-
 # TODO: should be able to start and stop the OPC on very long sampling intervals
 
 # --------------------------------------------------------------------------------------------------------------------
