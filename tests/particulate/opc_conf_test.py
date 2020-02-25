9730610c5bfa65b2091bbb8c3a667e253608ad57
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-02-25 11:00:49 +0000

    Building scs_core exegesis stubs

--- tests/particulate/opc_conf_test.py
+++ tests/particulate/opc_conf_test.py
@@ -8,9 +8,6 @@ Created on 23 Nov 2019
 
 from scs_core.data.json import JSONify
 
-from scs_core.particulate.exegesis.iselut.iselut_n2_v001 import ISELUTN2v1
-from scs_core.particulate.exegesis.isecee.isecee_n2_v001 import ISECEEN2v1
-
 from scs_dfe.particulate.opc_conf import OPCConf
 
 from scs_host.sys.host import Host
@@ -18,7 +15,7 @@ from scs_host.sys.host import Host
 
 # --------------------------------------------------------------------------------------------------------------------
 
-conf = OPCConf("N2", 10, False, 0, 1, [ISELUTN2v1.name(), ISECEEN2v1.name()])
+conf = OPCConf("N2", 10, False, 0, 1, [])
 print(conf)
 print("-")
 
