c095af4b027de538ece0983b2be2d0073721027a
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-04-21 13:47:26 +0100

    Removed TODOs

--- src/scs_dfe/particulate/opc_monitor.py
+++ src/scs_dfe/particulate/opc_monitor.py
@@ -19,8 +19,6 @@ from scs_dfe.particulate.opc import OPC
 from scs_host.lock.lock_timeout import LockTimeout
 
 
-# TODO: should be able to start and stop the OPC on very long sampling intervals
-
 # --------------------------------------------------------------------------------------------------------------------
 
 class OPCMonitor(SynchronisedProcess):
