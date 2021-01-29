4130e46c42f50329382e002eecec8e0eef674123
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-01-29 07:55:35 +0000

    Added __eq__(..) methods

--- src/scs_dfe/gas/scd30/scd30_conf.py
+++ src/scs_dfe/gas/scd30/scd30_conf.py
@@ -7,40 +7,17 @@ example JSON:
 {"sample-interval": 2, "temp-offset": 0.0}
 """
 
-from collections import OrderedDict
-
-from scs_core.data.datum import Datum
-from scs_core.data.json import PersistentJSONable
+from scs_core.gas.scd30.scd30_conf import SCD30Conf as AbstractSCD30Conf
 
 from scs_dfe.gas.scd30.scd30 import SCD30
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class SCD30Conf(PersistentJSONable):
+class SCD30Conf(AbstractSCD30Conf):
     """
     classdocs
     """
-
-    __FILENAME = "scd30_conf.json"
-
-    @classmethod
-    def persistence_location(cls):
-        return cls.conf_dir(), cls.__FILENAME
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    @classmethod
-    def construct_from_jdict(cls, jdict):
-        if not jdict:
-            return None
-
-        sample_interval = jdict.get('sample-interval')
-        temperature_offset = jdict.get('temp-offset')
-
-        return cls(sample_interval, temperature_offset)
-
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -48,8 +25,7 @@ class SCD30Conf(PersistentJSONable):
         """
         Constructor
         """
-        self.__sample_interval = Datum.int(sample_interval)                     # int       seconds
-        self.__temperature_offset = Datum.float(temperature_offset, 1)          # float     °C
+        super().__init__(sample_interval, temperature_offset)
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -61,29 +37,6 @@ class SCD30Conf(PersistentJSONable):
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def as_json(self):
-        jdict = OrderedDict()
-
-        jdict['sample-interval'] = self.sample_interval
-        jdict['temp-offset'] = self.temperature_offset
-
-        return jdict
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    @property
-    def sample_interval(self):
-        return self.__sample_interval
-
-
-    @property
-    def temperature_offset(self):
-        return self.__temperature_offset
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
     def __str__(self, *args, **kwargs):
-        return "SCD30Conf:{sample_interval:%s, temperature_offset:%s}" %  \
+        return "SCD30Conf(dfe):{sample_interval:%s, temperature_offset:%s}" %  \
                (self.sample_interval, self.temperature_offset)
