a02b82da8ace8cbb8de41e12552ee2174eb758c3
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-11-02 11:06:10 +0000

    Refactored configuration document

--- tests/gas/afe_baseline_test.py
+++ tests/gas/afe_baseline_test.py
@@ -10,7 +10,7 @@ from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.json import JSONify
 
 from scs_core.gas.afe_baseline import AFEBaseline
-from scs_core.gas.sensor_baseline import SensorBaseline, BaselineEnvironment
+from scs_core.gas.sensor_baseline import SensorBaseline
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -22,10 +22,10 @@ print("-")
 now = LocalizedDatetime.now().utc()
 
 baseline = AFEBaseline([
-    SensorBaseline(now, 111, BaselineEnvironment(1.1, 2.2, 3.3)),
-    SensorBaseline(now, 222, BaselineEnvironment(11.1, 22.2, 33.3)),
-    SensorBaseline(now, 333, BaselineEnvironment(111.1, 222.2, 333.3)),
-    SensorBaseline(now, 444, BaselineEnvironment(1111.1, 2222.2, 3333.3))])
+    SensorBaseline(now, 111),
+    SensorBaseline(now, 222),
+    SensorBaseline(now, 333),
+    SensorBaseline(now, 444)])
 
 print(baseline)
 print("-")
