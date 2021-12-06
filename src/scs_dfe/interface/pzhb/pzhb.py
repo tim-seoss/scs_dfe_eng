05708b27ba65438d11fad0d947bcff3df37dc87d
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-12-06 13:53:31 +0000

    Fixed handling of missing AFECalib

--- src/scs_dfe/interface/pzhb/pzhb.py
+++ src/scs_dfe/interface/pzhb/pzhb.py
@@ -45,8 +45,11 @@ class PZHB(Interface):
     def gas_sensors(self, host):
         # sensors...
         afe_calib = AFECalib.load(host)
-        afe_baseline = AFEBaseline.load(host, skeleton=True)
 
+        if afe_calib is None:
+            return None
+
+        afe_baseline = AFEBaseline.load(host, skeleton=True)
         sensors = afe_calib.sensors(afe_baseline)
 
         return ISI(sensors)
